9276a0871388e763650ab99ff8e7b906f9e2ecb9
KienDinhVan <130131199+KienDinhVan@users.noreply.github.com>
2023-12-29 20:35:22 +0700

    Update LinReg.py

--- LinReg.py
+++ LinReg.py
@@ -3,12 +3,12 @@ from sklearn.linear_model import LinearRegression
 from split_train_test import create_train_test_test
 
 def Lr_implement(df_Stock,your_data):
-    X_train, X_test, Y_train, Y_test = create_train_test_test(df_Stock)
-    lr = LinearRegression()
+    X_train, X_test, Y_train, Y_test = create_train_test_test(df_Stock) # Split train/test set
+    lr = LinearRegression() # Implement model
     lr.fit(X_train, Y_train)
-    y_pred = lr.predict(X_test)
-    accuracy = 1 - np.mean(np.abs((y_pred - Y_test) / Y_test))
-    result = lr.predict(your_data)
+    y_pred = lr.predict(X_test) # Predict test set
+    accuracy = 1 - np.mean(np.abs((y_pred - Y_test) / Y_test)) # Test for accuracy
+    result = lr.predict(your_data) # Predict base on user data
     return accuracy, result
 
 
