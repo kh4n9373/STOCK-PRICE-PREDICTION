f2f6b49dc51f2dfe31b70060ea00521fb574a1c1
KienDinhVan <130131199+KienDinhVan@users.noreply.github.com>
2023-12-29 21:37:19 +0700

    Update crawling_data.py

--- crawling_data.py
+++ crawling_data.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 #importing yfinance api
 import yfinance as yf
 import pandas as pd
@@ -15,11 +9,7 @@ api_key = "143c9a19a5a05044020a48c6de25ad81"
 # Set the data source as "fred" 
 pdr.fred.FredReader.api_key = api_key
 
-
-# In[8]:
-
-
-#crawling data from yFinance
+# Crawling data from yFinance
 
 # def fetch_stock_data(symbol, period):
 #     stock = yf.Ticker(symbol)
@@ -38,39 +28,16 @@ def fetch_stock_data(symbol, period):
     except Exception as e:
         raise ValueError("THERE IS NO SUCH BUSINESS CODE !!") from e
 
-
-# In[4]:
-
-
-#saving the data into csv
+# Saving the data into csv
 def save_to_csv(data, symbol):
     file_name = f"{symbol}_historical_data_yfinance.csv"
     data.to_csv(file_name, index=False)
-
-
-# In[9]:
-
-
-#changing symbol (stock name) and period to get the csv file with the data
-# symbol = "MSFT"
-# period = "10y"
-# stock_data = fetch_stock_data(symbol,period)
-# save_to_csv(stock_data, symbol)
-
-
-# In[5]:
-
 
 #crawling data from FRED
 def fetch_and_save_data(series_name, start_date, end_date):
     data = pdr.get_data_fred(series_name, start_date, end_date)
     file_name = f"{series_name}_data.csv"
     data.to_csv(file_name)
-
-
-
-# In[6]:
-
 
 #changing series name, start and end date to get the desired data in a csv file
 # series_name = "GDP"
